5f5cccd3b495edbbe5a00280555f5ab50cae9ce8
Alexei <talanovalexej@yandex.ru>
2023-05-02 12:11:01 +0300

    Shell sort complete

--- sortShell.py
+++ sortShell.py
@@ -2,7 +2,7 @@
 import random
 import time
 
-n = 5000
+n = 50
 arr = list()
 for i in range(n):
     number = random.randint(1, 100)
@@ -18,10 +18,14 @@ start = time.time()
 step = len(arr) // 2
 while step > 0:
     for i in range(step, n, 1):
-        value = arr[i]
         current_index = i
         index_to_check = current_index - step
-
+        while (current_index > 0 and 
+               arr[index_to_check] > arr[current_index]):
+            arr[current_index], arr[index_to_check] = arr[index_to_check], arr[current_index]
+            current_index -= step
+            index_to_check -= step
+    step = step // 2
 
 
 ###########################################
