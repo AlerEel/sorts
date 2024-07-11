56852c52c900198922215cdd9ded527b98c2a93a
Aler <talanovalexej@mail.ru>
2024-07-11 13:40:54 +0300

    The order has been restored

--- factorial.py
+++ factorial.py
@@ -4,43 +4,46 @@ import time
 
 #Факториал без рекурсии
 
-# n = 1234
-# fact = 1
+def fact(n):
+    fact = 1
+    for i in range(1, n + 1, 1):
+        fact *= i
+    return fact
 
-# for i in range(1, n + 1, 1):
-#     fact *= i
-# print(f"Факториал {n} = {fact}")
 
 #Факториал c рекурсией
 
-# def fact_r(n):
-#     if n <= 1:
-#         return 1
-#     return n * fact_r(n-1)
-
-# n = 195789
-# fact = fact_r(n)
-# print(f"Факториал {n} = {fact}")
+def fact_r(n):
+    if n <= 1:
+        return 1
+    return n * fact_r(n-1)
 
 #Числа фибоначи
-start = time.time()
-
-
-
 def fibonacci(n):
-    
-    if n <= 2:
-        return 1
-    
-    return fibonacci(n-1) + fibonacci(n-2)
+    if n > 1:
+        if n <= 2:
+            return 1
+        return fibonacci(n - 1) + fibonacci(n - 2)
+    else: return 0
 
-n=10
 
-print(fibonacci(n))
+n=15
 
+start = time.time()
+print(f"Число фибоначчи № {n} = {fibonacci(n)}")
 end = time.time()
-
 print("The time of execution of above program is :",
       (end-start) * 10**3, "ms")
 
-# The time of execution of above program is : 11426.26404762268 ms
+# # The time of execution of above program is : 11426.26404762268 ms
+# start = time.time()
+# print(f"Факториал {n} = {fact(n)}")
+# end = time.time()
+# print("The time of execution of above program is :",
+#       (end-start) * 10**3, "ms")
+#
+# start = time.time()
+# print(f"Факториал {n} = {fact_r(n)}")
+# end = time.time()
+# print("The time of execution of above program is :",
+#       (end-start) * 10**3, "ms")
--- sortShell.py
+++ sortShell.py
@@ -1,36 +1,30 @@
-# Shell sort, Сортировка Шелла
 import random
 import time
+start = time.time()
 
-n = 50
+n=500000
 arr = list()
+
 for i in range(n):
     number = random.randint(1, 100)
     arr.append(number)
 
-print('Not sorted')
-print(arr)
-print("---------")
+def shell_sort(arr):
+    step = len(arr) // 2
+    while step > 0:
+        for i in range(step, n, 1):
+            current_index = i
+            index_to_check = current_index - step
+            while (current_index > 0 and
+                   arr[index_to_check] > arr[current_index]):
+                arr[current_index], arr[index_to_check] = arr[index_to_check], arr[current_index]
+                current_index -= step
+                index_to_check -= step
+        step = step // 2
+    return arr
 
-start = time.time()
-#########################################
-
-step = len(arr) // 2
-while step > 0:
-    for i in range(step, n, 1):
-        current_index = i
-        index_to_check = current_index - step
-        while (current_index > 0 and 
-               arr[index_to_check] > arr[current_index]):
-            arr[current_index], arr[index_to_check] = arr[index_to_check], arr[current_index]
-            current_index -= step
-            index_to_check -= step
-    step = step // 2
-
-
-###########################################
+print('Sorted:')
+print(shell_sort(arr))
 end = time.time()
-print('Sorted')
-print(arr)
 print("The time of execution of above program is :",
       (end-start) * 10**3, "ms")
